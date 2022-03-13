a3e5e59caada5033fc300329c83cb1204345253a
Shravan Santosh Shinde <83361028+shravanshinde@users.noreply.github.com>
2022-03-13 17:21:27 +0530

    Add files via upload

---
+++ src/coffe/products.js
@@ -0,0 +1,43 @@
+export const products = [
+  {
+    id: 1,
+    title: "Chocolate Truffle",
+    description: "black in color",
+    price: "25",
+    img: "https://imgcdn.floweraura.com/sinful-chocolate-truffle-a.jpg",
+    quantity: 10,
+  },
+  {
+    id: 2,
+    title: "Choco Cherry Cake half kg",
+    description: "black+ white in color",
+    price: "23",
+    img: "https://www.fnp.com/images/pr/l/v20171214164135/choco-cherry-cake-half-kg_1.jpg",
+    quantity: 10,
+  },
+  {
+    id: 3,
+    title: "Choco Cherry Cake half kg",
+    description: "black+ white in color",
+    price: "23",
+    img: "https://www.fnp.com/images/pr/l/v20171214164135/choco-cherry-cake-half-kg_1.jpg",
+    quantity: 10,
+  },
+  {
+    id: 4,
+    title: "Chocolate Truffle",
+    description: "black in color",
+    price: "25",
+    img: "https://imgcdn.floweraura.com/sinful-chocolate-truffle-a.jpg",
+    quantity: 10,
+  },
+  {
+    id: 5,
+    title: "Classic Choco Chip Cake 1KG",
+    description: "black in color",
+    price: "35",
+    img: "https://www.fnp.com/images/pr/l/v20170426125427/classic-choco-chip-cake-1kg_1.jpg",
+    quantity: 10,
+  },
+ 
+];
