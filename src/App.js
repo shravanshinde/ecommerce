e654c8deb509c7a034366e166fb8dac379b90d75
Shravan Santosh Shinde <83361028+shravanshinde@users.noreply.github.com>
2022-03-13 17:20:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,93 @@
+// // import { Button } from 'bootstrap'; 
+// import React from 'react';
+// import  './App.css';
+// // import Main from './components/Main';
+// // import Button from './components/Button/Button.js';
+// // import Add from './components/addAmount/Add';
+// // import Details from './components/details/Details';
+// // import Header from './components/header/Header';
+// // import Container from './components/Grid/Container';
+// // import Card from './components/Card';
+// // import CardComp from './components/CardComponent.js'
+// // import CardComp from './Componentss/CardComponenent';
+// // import HomeCard from './Componentss/HomeCard';
+// // import Home from "./Componentss/Coomunication/Home/Home";  
+// // import Parent from './Componentss/Parent-Child/Parent';
+// // import A from './Componentss/Child-Parent/A';
+// // import Mounting from './Lifecycle/Mounting';
+// // import Updating from './Lifecycle/Updating';
+// // import Home from './React-Router-dom/Home';
+// //  import About from './React-Router-dom/About';
+// // import Contact from './React-Router-dom/Contact';
+// // import {Route, Routes} from 'react-router-dom';
+// // import Navigation from './Navigation/Navigation';
+
+
+// class Abdul extends React.Component {
+//   render(){
+   
+
+//     return (
+//       <div className="App">
+//         {/* <Navigation/>
+//      <Routes>
+//        <Route path='/about' element={<About/>}/>
+//        <Route path='/home' element={<Home/>}/>
+//        <Route path='/contact' element={<Contact/>}/>
+//      </Routes>
+//       */}
+
+
+
+
+     
+//       {/* {<Main/>}
+
+
+//       {<Button/>} 
+//       <Container/>
+//       <Card/>
+//       <CardComp/>
+//       <HomeCard
+//       <Parent/>
+//       <A/>
+//       <Home/>
+//       <Mounting/>
+//       <Updating/>
+      
+//       <About/>
+//       */
+    
+    
+//     }
+  
+     
+//       {/* <Header/>
+//       <Add/>
+//     <Details/>
+//     <Home/>
+//      */
+//     } 
+//     {/* {< Contact/>} */}
+//     </div>
+//   );
+
+// }
+// }
+
+// export default Abdul;
+
+
+import React from "react";
+// import Cart from "./component/Cart";
+import Cart from "./coffe/Cart";
+
+const App = () => {
+  return (
+    <>
+      <Cart />
+    </>
+  );
+};
+
+export default App;
